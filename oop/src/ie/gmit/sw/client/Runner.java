172bd888e3f4f3a3b8821f6df82395c2e5455fe1
TangqiFeng <kylezete@gmail.com>
2017-01-03 22:18:38 +0000

    add download function between server and client

--- oop/src/ie/gmit/sw/client/Runner.java
+++ oop/src/ie/gmit/sw/client/Runner.java
@@ -1,5 +1,8 @@
 package ie.gmit.sw.client;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -14,13 +17,13 @@ public class Runner {
 	Parseator p;
 	Context ctx;
 	String message = ""; // store received message
-	UI cin = new UI();  // input from keyboard
-	String s="";   //input String from keyboard
-	
-	public void run() throws Throwable{
+	UI cin = new UI(); // input from keyboard
+	String s = ""; // input String from keyboard
+	Boolean flag = true;
+
+	public void run() throws Throwable {
 		try {
-			Boolean flag = true;
-			while(flag){
+			while (flag) {
 				System.out.println("1. Connect to Server");
 				System.out.println("2. Print File Listing");
 				System.out.println("3. Download File");
@@ -28,41 +31,44 @@ public class Runner {
 				System.out.println();
 				System.out.println("Type Option [1-4]>");
 				s = cin.getString();
-				while(true){
-					if(s.equals("1")){
-						if(ss == null){
-						System.out.println("111111111");
-						connection();
-						//System.out.println(ctx);
-						System.out.println();
-					}else{
-						System.out.println("<<ERROR>>");
-						System.out.println("         You have already connected");
-						System.out.println("                                    !!!");
-						System.out.println();
-					}
+				while (true) {
+					if (s.equals("1")) {
+						if (ss == null) {
+							System.out.println("111111111");
+							connection();
+							// System.out.println(ctx);
+							System.out.println();
+						} else {
+							System.out.println("<<ERROR>>");
+							System.out.println("         You have already connected");
+							System.out.println("                                    !!!");
+							System.out.println();
+						}
 						break;
-						
-					}else if(s.equals("2")){
-						if(ss == null){
+
+					} else if (s.equals("2")) {
+						if (ss == null) {
 							System.out.println("<<ERROR>>");
 							System.out.println("You need connect first!   try again>");
 							System.out.println();
-						}else{
+						} else {
 							getList();
 						}
 						break;
-					}else if(s.equals("3")){
-						if(ss == null){
+					} else if (s.equals("3")) {
+						if (ss == null) {
 							System.out.println("<<ERROR>>");
-							System.out.println("You need connect first!   try again>"); 
+							System.out.println("You need connect first!   try again>");
 							System.out.println();
+						} else {
+							downloadFile();
 						}
-					}else if(s.equals("4")){
+						break;
+					} else if (s.equals("4")) {
 						System.out.println("bye-bye  @.@");
 						flag = false;
 						break;
-					}else{
+					} else {
 						System.out.println("<<ERROR>>");
 						System.out.println("           Please input [1-4]>");
 						System.out.println();
@@ -72,7 +78,7 @@ public class Runner {
 			}
 		} catch (ClassNotFoundException | IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// e.printStackTrace();
 		} finally {
 			// 4: Closing connection
 			try {
@@ -80,12 +86,12 @@ public class Runner {
 				out.close();
 				requestSocket.close();
 			} catch (IOException ioException) {
-				ioException.printStackTrace();
+				// ioException.printStackTrace();
 			}
 		}
 	}
-	
-	void connection() throws Throwable{
+
+	void connection() throws Throwable {
 		try {
 
 			// get context object from conf.xml
@@ -103,39 +109,88 @@ public class Runner {
 			in = new ObjectInputStream(requestSocket.getInputStream());
 			// 3. Communicating with the server
 			message = (String) in.readObject();
-			System.out.println("<<<<< "+message+" >>>>>");
-			ss = "ok";  // used to ignore repeat connection
-			
+			System.out.println("<<<<< " + message + " >>>>>");
+			ss = "ok"; // used to ignore repeat connection
+
 		} catch (UnknownHostException unknownHost) {
 			System.err.println("You are trying to connect to an unknown host!");
 		} catch (IOException ioException) {
-			ioException.printStackTrace();
-		} 
+			// ioException.printStackTrace();
+		}
 	}
-	
+
 	void sendMessage(String msg) {
 		try {
 			out.writeObject(msg);
 			out.flush();
 			System.out.println("client> " + msg);
 		} catch (IOException ioException) {
-			ioException.printStackTrace();
+			// ioException.printStackTrace();
 		}
 	}
-	
-	void getList() throws ClassNotFoundException, IOException{
+
+	void getList() throws ClassNotFoundException, IOException {
 		sendMessage("list");
 		message = (String) in.readObject();
-		int sum = Integer.parseInt(message); //get the amount of files
+		int sum = Integer.parseInt(message); // get the amount of files
 		System.out.println("file list :");
 		System.out.println();
-		for (int i = 0; i < sum; i++){
+		for (int i = 0; i < sum; i++) {
 			message = (String) in.readObject();
 			System.out.println(message);
 		}
 		System.out.println();
 	}
-	
+
+	void downloadFile() throws ClassNotFoundException, IOException {
+		sendMessage("download");
+		while (true) {
+			System.out.println("Please input the fileName you want to download>");
+			s = cin.getString();
+			sendMessage(s);
+			message = (String) in.readObject();
+			if (message.equals("ok")) {
+				break;
+			}
+			if (message.equals("error")) {
+				System.out.println("==---------NO THIS FILE !!!--------==");
+			}
+		}
+		sendMessage(s);
+		receiveFile();
+	}
+
+	void receiveFile() throws IOException {
+		byte[] inputByte = null;
+		int length = 0;
+		try {
+
+			FileOutputStream fout = new FileOutputStream(new File(ctx.getDownload_dir() + in.readUTF()));
+			inputByte = new byte[1024];
+			System.out.println("start doenload...");
+			System.out.println("..............");
+			while (true) {
+				if (in != null) {
+					length = in.read(inputByte, 0, inputByte.length);
+				}
+				if (length == -1) {
+					break;
+				}
+				System.out.println(length);
+				fout.write(inputByte, 0, length);
+				fout.flush();
+			}
+			System.out.println("complete download");
+			System.out.println("...................");
+			System.out.println("bye-bye  @.@");
+			fout.close();
+			flag = false;
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
 	public static void main(String args[]) throws Throwable {
 		Runner r = new Runner();
 		r.run();
--- oop/src/ie/gmit/sw/server/ClientServiceThread.java
+++ oop/src/ie/gmit/sw/server/ClientServiceThread.java
@@ -1,11 +1,12 @@
 package ie.gmit.sw.server;
 
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-
 
 class ClientServiceThread extends Thread {
 
@@ -29,39 +30,100 @@ class ClientServiceThread extends Thread {
 			ioException.printStackTrace();
 		}
 	}
-	
-	public void run(){
+
+	public void run() {
 		try {
 			out = new ObjectOutputStream(clientSocket.getOutputStream());
 			out.flush();
 			in = new ObjectInputStream(clientSocket.getInputStream());
-			System.out.println("Accepted Client : ID - " + clientID + " : Address - "
+			System.out.println("Accepted Client : ID - " + clientID
+					+ " : Address - "
 					+ clientSocket.getInetAddress().getHostName());
 			sendMessage("Connection successful");
-			
+
 			askOperation();
-			
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//e.printStackTrace();
+			System.out.println("Client : ID - " + clientID + "   exit !...");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	void askOperation() throws IOException, ClassNotFoundException{
-		message = (String) in.readObject();
-		if(message.equals("list")){
-			String[] str = new File("src//ie//gmit//sw//server//files//" + File.separator).list();
-			sendMessage(str.length+"");
-			for (int i = 0; i < str.length; i++) {
-				System.out.println(str[i]);
-				sendMessage(str[i]);
+			//e.printStackTrace();
+		}finally {
+			// 4: Closing connection
+			try {
+				in.close();
+				out.close();
+				clientSocket.close();
+			} catch (IOException ioException) {
+				// ioException.printStackTrace();
 			}
-			
 		}
 	}
-	
+
+	void askOperation() throws IOException, ClassNotFoundException {
+		while (true) {
+			message = (String) in.readObject();
+			if (message.equals("list")) {
+				getList();
+			}
+			if (message.equals("download")) {
+				downloadFile();
+			}
+		}
+	}
+
+	void downloadFile() throws IOException, ClassNotFoundException {
+		String signal = "no"; // check whether the file client need is exist
+		String fileName;
+		while (true) {
+			fileName = (String) in.readObject();
+			String[] str = new File("src//ie//gmit//sw//server//files//"
+					+ File.separator).list();
+			for (int i = 0; i < str.length; i++) {
+				if (str[i].equals(fileName)) {
+					signal = "yes";
+					break;
+				}
+			}
+			if (signal.equals("yes")) {
+				sendMessage("ok");
+				break;
+			} else {
+				sendMessage("error");
+			}
+		}
+		fileName = (String) in.readObject();
+		sendFile(fileName);
+	}
+
+	void sendFile(String filename) throws IOException {
+		int length = 0;
+		byte[] sendByte = null;
+		DataOutputStream dout = null;
+		FileInputStream fin = null;
+		dout = new DataOutputStream(out);
+		File file = new File("src//ie//gmit//sw//server//files//"+filename);
+		fin = new FileInputStream(file);
+		sendByte = new byte[1024];
+		dout.writeUTF(file.getName());
+		while ((length = fin.read(sendByte, 0, sendByte.length)) > 0) {
+			dout.write(sendByte, 0, length);
+			dout.flush();
+		}
+		fin.close();
+		dout.close();
+	}
+
+	void getList() {
+		String[] str = new File("src//ie//gmit//sw//server//files//"
+				+ File.separator).list();
+		sendMessage(str.length + "");
+		for (int i = 0; i < str.length; i++) {
+			// System.out.println(str[i]);
+			sendMessage(str[i]);
+		}
+	}
+
 }
